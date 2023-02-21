ea16f82f700a4f988f4bcbe84e9b718301b6639d
Vikas <vikas1618072@gmail.com>
2023-02-21 16:30:08 +0530

    added + and -quantity

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
 from app import schemas, models
@@ -34,9 +34,11 @@ async def add_item_to_cart(item: schemas.CartCreate, db: Session = Depends(get_d
 
 @router.put("/carts/{cart_id}/items/{item_id}", response_model=schemas.Cart)
 async def update_cart_item(cart_id: int, item_id: int, item: schemas.CartCreate, db: Session = Depends(get_db)):
+    
     cart = db.query(models.Cart).filter(models.Cart.id == cart_id).first()
     if not cart:
         raise HTTPException(status_code=404, detail="Cart not found")
+    
     cart_item = db.query(models.Cart).filter(models.Cart.id == item_id).first()
     if not cart_item:
         raise HTTPException(status_code=404, detail="Cart item not found")
@@ -62,3 +64,49 @@ async def delete_cart_item(cart_id: int, item_id: int, db: Session = Depends(get
     db.delete(cart_item)
     db.commit()
     return {"message": "Cart item deleted successfully"}
+
+
+
+@router.put('/cart/increase/quantity/{cart_id}')
+async def increase_item_quantity(cart_id: int, db: Session=Depends(get_db)):
+    # retrieve the cart item from the database
+    cart_item = db.query(models.Cart).filter(models.Cart.id == cart_id).first()
+    
+    # check if the item exists
+    if not cart_item:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cart item not found")
+    
+    # increment the quantity of the item
+    cart_item.quantity += 1
+    
+    # update the database
+    db.add(cart_item)
+    db.commit()
+    db.refresh(cart_item)
+    
+    # return the updated cart item
+    return cart_item
+
+
+@router.put('/cart/decrease/quantity/{cart_id}')
+async def decrease_item_quantity(cart_id: int, db: Session=Depends(get_db)):
+    # retrieve the cart item from the database
+    cart_item = db.query(models.Cart).filter(models.Cart.id == cart_id).first()
+    
+    # check if the item exists
+    if not cart_item:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cart item not found")
+    
+    # check if the quantity is already 1
+    if cart_item.quantity == 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Quantity cannot be less than 1")
+    
+    # decrement the quantity of the item
+    cart_item.quantity -= 1
+    
+    # update the database
+    db.add(cart_item)
+    db.commit()
+    db.refresh(cart_item)
+    
+    return cart_item
